3bebe0f6d7f14fdffc19a54f343b182e0e83acf3
Bojan Simoski <bojan.2110@gmail.com>
2019-03-09 19:06:18 +0100

    microphone support for insertmany, differentiate bluetooth routes

--- controllers/microphone.controller.js
+++ controllers/microphone.controller.js
@@ -18,17 +18,37 @@ exports.index = function (req, res) {
 };
 // Handle create contact actions
 exports.new = function (req, res) {
-    var micentry = new MicrophoneEntry();
-    micentry.speakStatus = req.body.speakStatus ? req.body.speakStatus : micentry.speakStatus;
-    micentry.collectionTime=req.body.collectionTime;
+//     var micentry = new MicrophoneEntry();
+//     micentry.speakStatus = req.body.speakStatus ? req.body.speakStatus : micentry.speakStatus;
+//     micentry.collectionTime=req.body.collectionTime;
+//
+// // save the contact and check for errors
+//     micentry.save(function (err) {
+//         // if (err)
+//         //     res.json(err);
+//         res.json({
+//             message: 'New Microphone Entry Created!',
+//             data: micentry
+//         });
+//     });
 
-// save the contact and check for errors
-    micentry.save(function (err) {
-        // if (err)
-        //     res.json(err);
-res.json({
-            message: 'New Microphone Entry Created!',
-            data: micentry
-        });
+    var data = []
+    for(const mic of req.body){
+      var micentry = new MicrophoneEntry();
+      micentry.speakStatus = mic.speakStatus;
+      micentry.collectionTime=mic.collectionTime;
+      data.push(micentry)
+    }
+    //save the contact and check for errors
+    MicrophoneEntry.insertMany(data,function (err) {
+        if (err)
+          {
+            res.json(err);
+          }
+          else{
+          res.json({
+              message: 'New Microphone Entry Created!'
+          });
+        }
     });
 };
--- routes/bluetooth.route.js
+++ routes/bluetooth.route.js
@@ -4,9 +4,8 @@ let router =require('express').Router();
 
 var bluetoothController = require('../controllers/bluetooth.controller');
 // Contact routes
-router.route('/bluetooth')
-    .get(bluetoothController.index)
-    .post(bluetoothController.new);
-    
+router.route('/bluetooth/get').get(bluetoothController.index);
+router.route('/bluetooth/post').post(bluetoothController.new);
+
 // Export API routes
 module.exports = router;
