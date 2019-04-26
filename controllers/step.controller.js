eee2cff974ccaeec6322dcad88a9970b28e2de91
Bojan Simoski <bojan.2110@gmail.com>
2019-04-26 17:10:01 +0200

    test steps

--- controllers/step.controller.js
+++ controllers/step.controller.js
@@ -29,6 +29,9 @@ exports.new = function (req, res) {
       stepentry.numSteps=step.numSteps;
       data.push(stepentry)
     }
+    console.log('step data sent')
+    console.log(data)
+
     //save the contact and check for errors
     Step.insertMany(data,function (err) {
         if (err)
@@ -36,6 +39,8 @@ exports.new = function (req, res) {
             res.json(err);
           }
           else{
+            console.log('step data inserted')
+            console.log(data)
             var ts=data.map(a => a.collectionTime)
             console.log('success : "Step Entries Inserted", status : 200')
             res.json({message : "Step Entries Inserted", status : 200,timestamps:ts});
