cae327b4df06ca94ab84fbc1dab618c16ae39cfb
Bojan Simoski <bojan.2110@gmail.com>
2019-04-28 16:04:40 +0200

    read error message

--- controllers/step.controller.js
+++ controllers/step.controller.js
@@ -39,13 +39,13 @@ exports.new = function (req, res) {
 
             var duplicates=JSON.stringify(err.writeErrors,undefined,2);
             // console.log(duplicates)
-            // var duplicates_ts=duplicates.map(function (el) { return el.name; });
+            var duplicates_ts=duplicates.map(function (el) { return el.collectionTime; });
             var all_ts=data.map(a => a.collectionTime)
-            console.log(all_ts)
-            // var res = duplicates_ts.filter( function(n) { return !this.has(n) }, new Set(all_ts) );
-            // console.log(duplicates_ts.length)
-            // console.log(all_ts.length)
-            // console.log(res.length)
+            // console.log(all_ts)
+            var res = duplicates_ts.filter( function(n) { return !this.has(n) }, new Set(all_ts) );
+            console.log(duplicates_ts.length)
+            console.log(all_ts.length)
+            console.log(res.length)
             // console.log(JSON.stringify(err.result,undefined,2));
             // console.log(JSON.stringify(err.result.insertedIds));
             //
