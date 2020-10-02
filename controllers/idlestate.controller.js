7e854cc69b94d6e20b3132d4d75c44fabe29c60c
Bojan Simoski <bojan.2110@gmail.com>
2020-10-02 16:35:43 +0200

    modifying IdleState objects 1

--- controllers/idlestate.controller.js
+++ controllers/idlestate.controller.js
@@ -20,7 +20,7 @@ exports.new = function (req, res) {
 
   var idlestate = new IdleState();
   idlestate.userid = req.body.userid;
-  idlestate.deviceid = req.body.userid;
+  idlestate.deviceid = req.body.deviceid;
   idlestate.collectionTime = req.body.collectionTime;
   idlestate.idleTime=req.body.idleTime;
 
