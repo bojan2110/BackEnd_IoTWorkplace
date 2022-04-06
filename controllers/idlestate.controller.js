5f4b26d4f81ff1364432bcbd99f6b6d6dec425c2
bojan2110 <bojan.2110@gmail.com>
2022-04-06 16:19:41 +0200

    AnalysisAPI1

--- controllers/idlestate.controller.js
+++ controllers/idlestate.controller.js
@@ -56,6 +56,66 @@ exports.getIdleStateDataPerLimit = function (req, res) {
     );
 
 };
+exports.getGluedCycle = function (req, res) {
+
+    var userid=req.params.userid;
+    // var deviceid=req.params.deviceid;
+    var startdate=req.params.startdate;
+    var enddate=req.params.enddate;
+
+    var findquery = {
+      "userid":userid,
+      "collectionTime": {"$lte":enddate,"$gte":startdate}
+    }
+    if(req.params.deviceid)
+    {
+      findquery.deviceid  = req.params.deviceid
+    }
+
+
+    IdleState.find(findquery,
+    function (err, idlestatedata) {
+        if (err) {
+          console.log('Error reading idle state data ',err)
+            res.json({
+                status: "error",
+                message: err,
+            });
+        }
+        else
+        {
+          if(idlestatedata === 'undefined')
+          {
+            console.log('idlestatedata undefined')
+            res.json({
+                status: "idlestatedata undefined",
+                intervals:[]
+            });
+          }
+          else if (idlestatedata.length == 0){
+            console.log('idlestatedata 0 ')
+            res.json({
+                status: "idlestatedata 0",
+                intervals:[]
+            });
+          }
+          else{
+
+           var intervalArray = createIntervals(idlestatedata,1,parseInt(startdate),parseInt(enddate))
+           var cyclesArray = getInferredCycles(intervalArray.intervals).cycles
+           var gluedCycles = glueCycles(cyclesArray)
+            res.json({
+                status: "success",
+                intervals:gluedCycles
+            });
+
+
+          }
+        }
+      }
+    );
+
+};
 
 exports.getIdleStateDataPerPeriod = function (req, res) {
 
