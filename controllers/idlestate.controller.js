3f1b341bfdc5c80851371adfe6072ba98832568c
bojan.2110 <bojan.2110@gmail.com>
2022-05-13 13:06:14 +0200

    test

--- controllers/idlestate.controller.js
+++ controllers/idlestate.controller.js
@@ -1,5 +1,100 @@
 IdleState = require('../models/idlestate.model');
+function createIntervals(states,slidingWindowMinutes,sTime,eTime){
 
+    // array that will contain the calculated intervals
+      var intervalArray = {
+          intervals: []
+      };
+
+      //interval length in seconds
+      var intervalLength = slidingWindowMinutes*60
+
+      // sort the intervals as sometimes app OFF is saved after app ON
+      states = states.sort(function(a, b) {  return a.collectionTime - b.collectionTime;});
+
+      var currentTime
+      var dayStart
+      if(sTime==undefined)
+      //get current timestamp
+      {
+        var start = new Date();
+        start.setHours(0,0,0,0);
+        dayStart =  Math.floor(start.getTime()/1000)
+      }
+      else
+        dayStart = sTime
+
+
+      if(eTime==undefined)
+      //get current timestamp
+      {
+        var end = new Date();
+        currentTime = Math.floor(end.getTime())/1000;
+      }
+      else
+        currentTime = eTime
+
+
+      // console.log('sTime dayStart eTime currentTime', sTime, dayStart, eTime, currentTime)
+
+        // these two are like interval slider start and end point
+      var intervalStart = dayStart
+      var intervalEnd = intervalStart + intervalLength - 1
+
+
+
+      //only if there are states to be calculated in the day
+      if(states.length != 0)
+      {
+
+            while(intervalStart<=currentTime){
+              //valid for the last interval in case that it exceed the currentTime
+              if(intervalEnd>currentTime)
+              {
+                    intervalEnd = currentTime
+              }
+
+              var slicedStates = states.filter(function (el) { return el.collectionTime >= intervalStart && el.collectionTime <= intervalEnd });
+              //there is data in the sliced interval
+              if(slicedStates.length !=0)
+                 slicedStates = slicedStates.sort(function(a, b) { return a.collectionTime - b.collectionTime;});
+
+              // the initial calculation
+              if(intervalArray.intervals.length == 0){
+                //the first sliced interval is empty - can happen very often
+                if(slicedStates.length == 0)
+                {
+                  intervalArray.intervals.push(getIntervalSummary(slicedStates,intervalStart,intervalEnd,10,slidingWindowMinutes))
+
+                }
+                else{
+                    intervalArray.intervals.push(getIntervalSummary(slicedStates,intervalStart,intervalEnd,states[0].idleTime,slidingWindowMinutes))
+
+                }
+
+              }
+              //intervalArray has already some calculated intervals
+              else{
+                    //the first state is the last state of the previous interval : intervalArray.intervals[intervalArray.intervals.length-1].lastState
+                    intervalArray.intervals.push(getIntervalSummary(slicedStates,intervalStart,intervalEnd, intervalArray.intervals[intervalArray.intervals.length-1].lastState, slidingWindowMinutes))
+
+              }
+
+                  intervalEnd = intervalEnd + intervalLength
+                  intervalStart = intervalStart + intervalLength
+            }
+
+
+      }
+      else{
+
+        console.log('No States Today')
+      }
+
+    return intervalArray
+
+
+  }
 exports.getIdleStateDataPerLimit = function (req, res) {
 
     var userid = req.params.userid;
